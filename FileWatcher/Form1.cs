a677da08f6a26a481b74496dbc4cd4c4ba713d91
psodev <psodev@ya.ru>
2017-11-25 14:22:00 +0300

    Start commit

--- FileWatcher/Form1.cs
+++ FileWatcher/Form1.cs
@@ -16,5 +16,10 @@ namespace FileWatcher
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
